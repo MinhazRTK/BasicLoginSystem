0897982cd3b68c30bcf8dd67c25330d939d787e5
minhazRTK <minhaz.rtk@gmail.com>
2018-08-13 17:34:10 +0600

    basic job done!

--- app/src/main/java/com/batch2/m0nk3y/basicloginsystem/MainActivity.java
+++ app/src/main/java/com/batch2/m0nk3y/basicloginsystem/MainActivity.java
@@ -1,7 +1,12 @@
 package com.batch2.m0nk3y.basicloginsystem;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText id = findViewById(R.id.username);
+        final EditText pass = findViewById(R.id.pass);
+        Button btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = id.getText().toString();
+                String password = pass.getText().toString();
+
+                if(username.equals("m0nk3y") && password.equals("admin")){
+                    Intent i = new Intent(MainActivity.this,LoggedIn.class);
+                    startActivity(i);
+                }
+                else {
+                    id.setText("wrong");
+                }
+            }
+        });
     }
 }
