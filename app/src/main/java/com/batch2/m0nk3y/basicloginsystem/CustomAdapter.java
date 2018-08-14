04974a9eb7c076feb21e96e58a2f12349d057b30
minhazRTK <minhaz.rtk@gmail.com>
2018-08-14 15:43:28 +0600

    list view added

---
+++ app/src/main/java/com/batch2/m0nk3y/basicloginsystem/CustomAdapter.java
@@ -0,0 +1,35 @@
+package com.batch2.m0nk3y.basicloginsystem;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.batch2.m0nk3y.basicloginsystem.R;
+
+public class CustomAdapter extends ArrayAdapter {
+
+    public CustomAdapter(@NonNull Context context, String[] mylist) {
+        super(context, R.layout.custom_row,mylist);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater customInflater = LayoutInflater.from(getContext());
+        View customView = customInflater.inflate(R.layout.custom_row,parent,false);
+
+        String mylist  = (String) getItem(position);
+        TextView textView = customView.findViewById(R.id.adaptertext);
+        ImageView imageView = customView.findViewById(R.id.adapterimage);
+
+        textView.setText(mylist);
+        imageView.setImageResource(R.drawable.m0nk3y);
+        return customView;
+    }
+}
--- app/src/main/java/com/batch2/m0nk3y/basicloginsystem/LoggedIn.java
+++ app/src/main/java/com/batch2/m0nk3y/basicloginsystem/LoggedIn.java
@@ -2,6 +2,12 @@ package com.batch2.m0nk3y.basicloginsystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class LoggedIn extends AppCompatActivity {
 
@@ -9,5 +15,19 @@ public class LoggedIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
+        final String[] mylist = {"Wake up","Brushing","Breakfast","Go to University","Take Lunch","Finish Lab","Return Home"};
+
+        //ListAdapter myListAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,mylist);
+        ListAdapter myListAdapter = new CustomAdapter(this,mylist);
+        ListView listView = findViewById(R.id.mylist);
+        listView.setAdapter(myListAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String mylist = String.valueOf(adapterView.getItemAtPosition(i));
+                Toast.makeText(LoggedIn.this, mylist, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
